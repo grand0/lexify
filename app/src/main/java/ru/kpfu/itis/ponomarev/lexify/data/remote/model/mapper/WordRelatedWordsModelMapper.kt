afca78c6dac0f0d5e6d417422c49a038a5f23b62
Ilya Ponomarev <iponom0420@gmail.com>
2024-04-10 01:23:41 +0300

    fix #5

--- app/src/main/java/ru/kpfu/itis/ponomarev/lexify/data/remote/model/mapper/WordRelatedWordsModelMapper.kt
+++ app/src/main/java/ru/kpfu/itis/ponomarev/lexify/data/remote/model/mapper/WordRelatedWordsModelMapper.kt
@@ -1,9 +1,7 @@
 package ru.kpfu.itis.ponomarev.lexify.data.remote.model.mapper
 
-import ru.kpfu.itis.ponomarev.lexify.data.remote.model.WordOfTheDayDataModel
 import ru.kpfu.itis.ponomarev.lexify.data.remote.model.WordRelatedWordsDataModel
 import ru.kpfu.itis.ponomarev.lexify.domain.model.RelatedWordsModel
-import ru.kpfu.itis.ponomarev.lexify.domain.model.WordOfTheDayModel
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,7 +9,7 @@ import javax.inject.Singleton
 class WordRelatedWordsModelMapper @Inject constructor() {
 
     fun mapDataModelToModel(data: WordRelatedWordsDataModel) = RelatedWordsModel(
-        relationshipType = data.relationshipType,
+        relationshipType = data.relationshipType.replace(Regex("[-_]"), " "),
         words = data.words,
     )
 }
