58b85bb77723af7d92e7683100ec1c31c36652ef
Ilya Ponomarev <iponom0420@gmail.com>
2024-04-09 19:27:51 +0300

    night mode switcher

--- app/src/main/java/ru/kpfu/itis/ponomarev/lexify/presentation/view/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/ponomarev/lexify/presentation/view/MainActivity.kt
@@ -2,10 +2,8 @@ package ru.kpfu.itis.ponomarev.lexify.presentation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.AndroidEntryPoint
-import ru.kpfu.itis.ponomarev.lexify.R
 import ru.kpfu.itis.ponomarev.lexify.databinding.ActivityMainBinding
 import ru.kpfu.itis.ponomarev.lexify.util.AppNavigator
 import javax.inject.Inject
@@ -25,6 +23,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         navigator.initNavController(binding.navHostFragmentContainer.id)
+
+        binding.ivNightModeBtn.setOnClickListener {
+            switchNightMode()
+        }
+    }
+
+    fun switchNightMode() {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 
     override fun onDestroy() {
